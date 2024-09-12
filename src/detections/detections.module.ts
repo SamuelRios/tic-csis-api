4f42ff888c6fef2a3a889427a1dfd31ac196fb1c
Samuel Rios <38793523+SamuelRios@users.noreply.github.com>
2024-09-12 15:50:30 +0000

    save config

--- src/detections/detections.module.ts
+++ src/detections/detections.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DetectionEntity } from './entities/detections.entity';
 import { DetectionsController } from './detections.controller';
 import { DetectionsService } from './detections.service';
 
-@Module({
+@Module({imports: [TypeOrmModule.forFeature([DetectionEntity])], // Aqui você registra a entidade
   controllers: [DetectionsController],
   providers: [DetectionsService]
 })

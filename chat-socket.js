eee236da62926f913cf111cd95654652adb420f5
Samuel Rios <mucarioss@gmail.com>
2024-10-04 15:03:09 -0300

    closed detection notification

--- chat-socket.js
+++ chat-socket.js
@@ -2,27 +2,46 @@
 import { io } from "https://cdn.socket.io/4.7.5/socket.io.esm.min.js";
 
 const socket = io("ws://localhost:3001", { transports : ['websocket'] })
+const detections = document.getElementById('detections');
 
-const message = document.getElementById('message');
-const messages = document.getElementById('messages');
-socket.on('detectionUpdate', (detection) => {
-  handleNewMessage(detection);
+socket.on('detectionCreated', (detection) => {
+  handleNewDetection(detection);
 })
 
 socket.on('activeDetections', (detections) => {
-  console.log(detections)
+  clearDetections();
   detections.forEach(detection => {
-    handleNewMessage(detection);
+    handleNewDetection(detection);
   });
 })
 
-const handleNewMessage = (detection) => {
-  messages.appendChild(buildNewMessage(detection));
+const handleNewDetection = (detection) => {
+  detections.appendChild(buildNewDetection(detection));
 }
 
-const buildNewMessage = (detection) => {
+socket.on('detectionClosed', (detectionId) => {
+  removeClosedDetection(detectionId);
+})
+
+const removeClosedDetection = (detectionId) => {
+  const items = detections.getElementsByTagName('li');
+  
+  for (let i = 0; i < items.length; i++) {
+    const item = items[i];
+    if (item.textContent.includes(`Detection id: ${detectionId}`)) {
+      detections.removeChild(item);
+      break;
+    }
+  }
+};
+
+const buildNewDetection = (detection) => {
   console.log(detection)
   const li = document.createElement("li");
   li.appendChild(document.createTextNode(`Detection id: ${ detection.detectionId}; Category: ${detection.category}; Status:${detection.status.statusId}; Timestamp:${detection.timestamp}`))
   return li;
+}
+
+const clearDetections = () => {
+    detections.innerHTML = '';
 }

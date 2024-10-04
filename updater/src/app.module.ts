f7e6eb62fbab28455a448dc61064950119e6ac73
Samuel Rios <mucarioss@gmail.com>
2024-10-04 09:33:20 -0300

    .env

--- updater/src/app.module.ts
+++ updater/src/app.module.ts
@@ -7,19 +7,27 @@ import { CameraLocationEntity } from './detections/entities/cameraLocation.entit
 import { UserEntity } from './detections/entities/user.entity';
 import { PriorityEntity } from './detections/entities/priority.entity';
 import { StatusEntity } from './detections/entities/status.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Isso garante que o ConfigModule será acessível em todo o projeto
+    }),
     DetectionsModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'dbadmin01',
-      database: 'detection_test',
-      entities: [DetectionEntity, CameraEntity, CameraLocationEntity, UserEntity, PriorityEntity, StatusEntity],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [DetectionEntity, CameraEntity, CameraLocationEntity, UserEntity, PriorityEntity, StatusEntity],
+        synchronize: true,
+      }),
     }),
   ],
   controllers: [],
